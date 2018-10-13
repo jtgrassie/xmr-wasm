ac94f7aaaa943ecb9ab4e8d43fa006f602262d2f
Jethro Grassie <jtg@xtrabass.com>
2018-10-13 11:16:52 -0400

    CNv2 update

--- src/crypto/random.h
+++ src/crypto/random.h
@@ -30,34 +30,6 @@
 
 #pragma once
 
-#if defined(__GNUC__)
-#if defined(__sun) && defined(__SVR4)
-#define INITIALIZER(name) __attribute__((constructor)) static void name(void)
-#define FINALIZER(name) __attribute__((destructor)) static void name(void)
-#else
-#define INITIALIZER(name) __attribute__((constructor(101))) static void name(void)
-#define FINALIZER(name) __attribute__((destructor(101))) static void name(void)
-#endif
-#define REGISTER_FINALIZER(name) ((void) 0)
+#include <stddef.h>
 
-#elif defined(_MSC_VER)
-#include <assert.h>
-#include <stdlib.h>
-// https://stackoverflow.com/questions/1113409/attribute-constructor-equivalent-in-vc
-// https://msdn.microsoft.com/en-us/library/bb918180.aspx
-#pragma section(".CRT$XCT", read)
-#define INITIALIZER(name) \
-  static void __cdecl name(void); \
-  __declspec(allocate(".CRT$XCT")) void (__cdecl *const _##name)(void) = &name; \
-  static void __cdecl name(void)
-#define FINALIZER(name) \
-  static void __cdecl name(void)
-#define REGISTER_FINALIZER(name) \
-  do { \
-    int _res = atexit(name); \
-    assert(_res == 0); \
-  } while (0);
-
-#else
-#error Unsupported compiler
-#endif
+void generate_random_bytes_not_thread_safe(size_t n, void *result);
